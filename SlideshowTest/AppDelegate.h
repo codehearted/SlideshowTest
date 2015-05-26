0e85ed1b7640883727cd7ad4014a9a9890f0e98f
TheoBendixson <theodorebendixson1@Theodores-iMac.local>
2015-05-26 10:11:29 -0600

    Initial Commit

---
+++ SlideshowTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideshowTest
+//
+//  Created by Theodore Bendixson on 5/26/15.
+//  Copyright (c) 2015 Theodore Bendixson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
